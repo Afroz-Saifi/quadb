deae1eaec8f790e52bda376840d5d177969cdb3b
Afroz-Saifi <afrozsaifi810@gmail.com>
2023-10-10 13:33:21 +0530

    created delte and get image routes

--- controller/users.cotroller.js
+++ controller/users.cotroller.js
@@ -1,6 +1,7 @@
 const { Users } = require("../models/index");
 const bcrypt = require("bcryptjs");
 
+// creating a new user
 const insertNewUser = async (req, res) => {
   try {
     const { user_password } = req.body;
@@ -18,4 +19,49 @@ const insertNewUser = async (req, res) => {
   }
 };
 
-module.exports = { insertNewUser };
+// deleting a users details
+const deleteUser = async (req, res) => {
+  try {
+    const data = await Users.destroy({
+      where: { user_id: req.params.id },
+    });
+    return res.status(200).json({
+      msg: "User deleted",
+      deleted_User: data,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      isError: true,
+      msg: error,
+    });
+  }
+}
+
+// getting image of a user
+const getImage = async (req, res) => {
+  try {
+    const {user_id} = req.params
+    const user = await Users.findOne({
+      attributes: ['user_image'],
+      where: {
+        "user_id": user_id,
+      },
+    });
+
+    if (user) {
+      return res.status(200).json({
+        user_image: user.user_image
+      })
+    } else {
+      return res.status(400).json({
+        error: "image not found"
+      })
+    }
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message
+    })
+  }
+}
+
+module.exports = { insertNewUser, deleteUser, getImage };
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,10 +1,17 @@
 const expres = require("express");
 const { alreadyExists } = require("../middleware/alreadyExists.middleware");
 const { checkEmailExistence } = require("../middleware/emailAuth.middleware");
-const { insertNewUser } = require("../controller/users.cotroller");
+const { insertNewUser, deleteUser, getImage } = require("../controller/users.cotroller");
 
 const UserRouter = expres.Router();
 
+// create a new user details
 UserRouter.post("/insert", alreadyExists, checkEmailExistence, insertNewUser);
+
+// delte a users profile
+UserRouter.delete('/delete/:id', deleteUser)
+
+// getting image of a user
+UserRouter.get("/image/:user_id", getImage)
 
 module.exports = { UserRouter };
